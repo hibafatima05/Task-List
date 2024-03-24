e7449210637844819bab6ba229bbed2960cdcbbb
hibafatima05 <hibarafeeq05@gmail.com>
2024-03-24 16:11:30 -0600

    second commit

---
+++ TaskList-App/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import NavBar from "./Components/Routes/Navbar";
+import Todo from "./Components/Todo/Todo";
+import Home from "./Components/Routes/Home";
+import Contacts from "./Components/Routes/Contact";
+import TaskDetails from "./Components/Todo/TaskDetails";
+const App = () => {
+  return (
+    
+      <>
+   APPpage
+    </>
+  );
+};
+
+export default App;
---
+++ TaskList-App/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { store } from './Components/store';
+ReactDOM.createRoot(document.getElementById('root')).render(
+<Provider store ={store}>
+  <Router>   
+    <App />
+  </Router>
+
+  
+</Provider>
+)
