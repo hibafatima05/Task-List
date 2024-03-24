7b46e6654444881c013440329804eca460868cae
hibafatima05 <hibarafeeq05@gmail.com>
2024-03-24 16:42:26 -0600

    Routes design

--- TaskList-App/src/App.jsx
+++ TaskList-App/src/App.jsx
@@ -1,16 +1,29 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./Routes/NavBar";
+import Home from "./Routes/Home";
+import Contacts from "./Routes/Contact";
 
-import NavBar from "./Components/Routes/Navbar";
-import Todo from "./Components/Todo/Todo";
-import Home from "./Components/Routes/Home";
-import Contacts from "./Components/Routes/Contact";
-import TaskDetails from "./Components/Todo/TaskDetails";
+// import NavBar from "./Components/Routes/Navbar";
+// import Todo from "./Components/Todo/Todo";
+// import Home from "./Components/Routes/Home";
+// import Contacts from "./Components/Routes/Contact";
+// import TaskDetails from "./Components/Todo/TaskDetails";
 const App = () => {
   return (
     
       <>
    APPpage
+   <NavBar/>
+   <Routes>
+    <Route/>
+<TASKMAIN/>
+   <Home/>
+   <Contacts/>
+   <SubTask/>
+
+   </Routes>
+   
     </>
   );
 };
--- TaskList-App/src/main.jsx
+++ TaskList-App/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { store } from './Components/store';
+import { store } from './Reducers/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
 <Provider store ={store}>
   <Router>   
